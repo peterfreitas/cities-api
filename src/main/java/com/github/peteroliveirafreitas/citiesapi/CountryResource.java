b0d7765dac109ca4d444ea13e580ff1702f4e05a
Peter Freitas <freitaspeteroliveira@gmail.com>
2021-10-29 12:57:40 -0300

    Implementação de Paginação

--- src/main/java/com/github/peteroliveirafreitas/citiesapi/CountryResource.java
+++ src/main/java/com/github/peteroliveirafreitas/citiesapi/CountryResource.java
@@ -3,6 +3,8 @@ package com.github.peteroliveirafreitas.citiesapi;
 import com.github.peteroliveirafreitas.citiesapi.countries.Country;
 import com.github.peteroliveirafreitas.citiesapi.repository.CountryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,11 +16,14 @@ import java.util.Scanner;
 @RequestMapping("/countries")
 public class CountryResource {
 
-    @Autowired
     private CountryRepository repository;
 
+    public CountryResource(CountryRepository repository) {
+        this.repository = repository;
+    }
+
     @GetMapping
-    public List<Country> countries(){
-        return repository.findAll();
+    public Page<Country> countries(Pageable page){
+        return repository.findAll(page);
     }
 }
