ea0f8bc2381d60f1396e77039e80f9ca0b9f4362
Peter Freitas <freitaspeteroliveira@gmail.com>
2021-10-29 13:20:27 -0300

    Implementação busca por id e retorno 404 quando não existir id

--- src/main/java/com/github/peteroliveirafreitas/citiesapi/CountryResource.java
+++ src/main/java/com/github/peteroliveirafreitas/citiesapi/CountryResource.java
@@ -2,15 +2,15 @@ package com.github.peteroliveirafreitas.citiesapi;
 
 import com.github.peteroliveirafreitas.citiesapi.countries.Country;
 import com.github.peteroliveirafreitas.citiesapi.repository.CountryRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-import java.util.Scanner;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/countries")
@@ -23,7 +23,19 @@ public class CountryResource {
     }
 
     @GetMapping
-    public Page<Country> countries(Pageable page){
+    public Page<Country> countries(Pageable page) {
         return repository.findAll(page);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getOne(@PathVariable Long id) {
+        Optional<Country> optional = repository.findById(id);
+
+        if (optional.isPresent()) {
+            return ResponseEntity.ok().body(optional.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+
     }
 }
