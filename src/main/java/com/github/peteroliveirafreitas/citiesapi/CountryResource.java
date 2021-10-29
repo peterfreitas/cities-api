4af365f358cf33eff06ff533085e1627324fb369
Peter Freitas <freitaspeteroliveira@gmail.com>
2021-10-29 10:25:20 -0300

    Implementação Controller com verbo GET, Classe Entidade e Repositorio.

---
+++ src/main/java/com/github/peteroliveirafreitas/citiesapi/CountryResource.java
@@ -0,0 +1,24 @@
+package com.github.peteroliveirafreitas.citiesapi;
+
+import com.github.peteroliveirafreitas.citiesapi.countries.Country;
+import com.github.peteroliveirafreitas.citiesapi.repository.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Scanner;
+
+@RestController
+@RequestMapping("/countries")
+public class CountryResource {
+
+    @Autowired
+    private CountryRepository repository;
+
+    @GetMapping
+    public List<Country> countries(){
+        return repository.findAll();
+    }
+}
